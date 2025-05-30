70fd4f006309da5e95f008704b3f23a327097556
tmxose <tmxose@naver.com>
2025-05-30 10:39:24 +0900

    shj add comment

--- Eco/src/main/java/com/eco/controller/SignupController.java
+++ Eco/src/main/java/com/eco/controller/SignupController.java
@@ -17,23 +17,18 @@ public class SignupController {
 	
 	private UserService service;
 	
-	
+	// 회원가입 페이지로 이동
 	@GetMapping("/signup")
 	public void signupPage() {
 		log.info("signup page");
 		
 	}
 	
+	// 회원가입 DB INSERT
 	@PostMapping("/signup")
 	public String signupPost(UserVO user) {
-		
 		log.info("signup Post");
-		log.info(user.getUser_id());
-		log.info(user.getUser_nm());		
-		log.info(user.getUser_pw());		
-		
 		service.signup(user);
-		
 		return "home";
 	}
 }
--- Eco/src/main/webapp/WEB-INF/views/index.jsp
+++ Eco/src/main/webapp/WEB-INF/views/index.jsp
@@ -4,9 +4,11 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>메인페이지</title>
 </head>
 <body>  
-
+	<div>
+		
+	</div>
 </body>
 </html>
