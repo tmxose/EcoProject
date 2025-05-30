3adc7ec748ce36994444686b5bfce5158e1cc77a
GRCOMNE <GRCOMNE@DESKTOP-41EUKUB>
2025-05-30 17:26:15 +0900

    pjh 한글 깨진거, db 수 증가에 따른 usage mapper 수정

--- Eco/src/main/java/com/eco/controller/IndexController.java
+++ Eco/src/main/java/com/eco/controller/IndexController.java
@@ -1,15 +1,14 @@
 package com.eco.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 public class IndexController {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public String Index() {
 		log.info("index 페이지 진입");
 		return "index";
