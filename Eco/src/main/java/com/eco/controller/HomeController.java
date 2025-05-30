ec57b38f4867c16bdb4e981f00d21cbcd341fe34
tmxose <tmxose@naver.com>
2025-05-30 10:54:06 +0900

    shj css and index page add

--- Eco/src/main/java/com/eco/controller/HomeController.java
+++ Eco/src/main/java/com/eco/controller/HomeController.java
@@ -8,13 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Log4j
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -36,4 +40,8 @@ public class HomeController {
 		return "home";
 	}
 	
+	@GetMapping("/index")
+	public void indexPage() {
+		log.info("index Page");
+	}
 }
