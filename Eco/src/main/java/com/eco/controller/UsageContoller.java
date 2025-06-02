0cb4f24a913cd73b37d24381a7971ba5a89f45e7
GRCOMNE <GRCOMNE@DESKTOP-41EUKUB>
2025-06-02 11:13:22 +0900

    pjh usage charge 컨트롤러 예외 처리

--- Eco/src/main/java/com/eco/controller/UsageContoller.java
+++ Eco/src/main/java/com/eco/controller/UsageContoller.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.eco.domain.UserTypeChargeDTO;
 import com.eco.domain.UserVO;
@@ -18,28 +19,46 @@ import lombok.extern.log4j.Log4j;
 @Controller
 @Log4j
 @AllArgsConstructor
+@RequestMapping("/usage")
 public class UsageContoller {
 	
 	private UsageService service;
 	
-	// 이번 달 나의 사용량 가져오기
-	@GetMapping("/usage")
+	// 이번 달 나의 에너지 사용량 가져오기
+	@GetMapping("")
 	public String usageSelect(Model model, HttpSession session) {
 		log.info("이번 달 나의 사용량 가져오기");
-		// session의 유저 아이디 가져오기
+		// session의 유저 정보 가져오기
 		UserVO user = (UserVO) session.getAttribute("currentUserInfo");
+		model.addAttribute("userName", user.getUser_nm());
 		// 이번 달 에너지 사용량 합계
-		model.addAttribute("usage", service.readGasusage(user.getUser_id()));
-		model.addAttribute("usage", service.readElecusage(user.getUser_id()));
-		log.info(user.getUser_id());
+		UserTypeChargeDTO gasTotal = service.readGasusage(user.getUser_id());
+		UserTypeChargeDTO elecTotal = service.readElecusage(user.getUser_id());
+		model.addAttribute("usage", gasTotal);
+		model.addAttribute("usage", elecTotal);
+		
+		// 이번 달 사용량이 0일 때 처리
+		if(gasTotal == null) {
+			model.addAttribute("gasUsageMsg", "이번 달 가스 사용량이 없습니다.");
+		}
+		if(elecTotal == null) {
+			model.addAttribute("elecUsageMsg", "이번 달 전기 사용량이 없습니다.");
+		}
+		
 		//가스 상세 내역
 		List<UserTypeChargeDTO> gasUse = service.gasUsageDetail(user.getUser_id());
 		model.addAttribute("gasUse", gasUse);
-		log.info(gasUse);
 		//전기 상세 내역
 		List<UserTypeChargeDTO> elecUse = service.elecUsageDetail(user.getUser_id());
 		model.addAttribute("elecUse", elecUse);
 		
 		return "usage";
 	}
+	
+	// 지정한 기간 나의 에너지 사용량 가져오기
+	@GetMapping("/period")
+	public void usageSelectPeriod() {
+		
+	}
+	
 }
