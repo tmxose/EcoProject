c9331cc428bd1d1efd05f17de827e7014147bbe3
GRCOMNE <GRCOMNE@DESKTOP-41EUKUB>
2025-06-02 12:21:14 +0900

    pjh usage, charge 컨트롤러 예외처리 전반 & 지정 기간 데이터 보기

--- Eco/src/main/java/com/eco/service/ChargeServiceImpl.java
+++ Eco/src/main/java/com/eco/service/ChargeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.eco.service;
 
+import java.time.LocalDate;
 import java.util.List; 
 
 import org.springframework.stereotype.Service;
@@ -13,26 +14,35 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ChargeServiceImpl implements ChargeService{
 	private ChargeMapper mapper;
-	// 이번 달 가스요금
+	
+	// 이번 달 에너지 요금 합계
 	@Override
 	public UserTypeChargeDTO readGasCharge(String userId) {
 		return mapper.getGasCharge(userId);
 	}
-	// 이번 달 전기요금
 	@Override
 	public UserTypeChargeDTO readElecCharge(String userId) {
 		return mapper.getElecCharge(userId);
 	}
 	
-	// 가스 상세 내역
+	// 당월 에너지 사용 상세 내역
 	@Override
 	public List<UserTypeChargeDTO> gasChargeDetail(String userId) {
 		return mapper.getGasChargeDetail(userId);
 	}
-	// 전기 상세 내역
 	@Override
 	public List<UserTypeChargeDTO> elecChargeDetail(String userId) {
 		return mapper.getElecChargeDetail(userId);
+	}
+	
+	// 지정 기간 에너지 사용 상세 내역
+	@Override
+	public List<UserTypeChargeDTO> gasChargePeriod(String userId, LocalDate startDate, LocalDate endDate) {
+		return mapper.getGasChargePeriod(userId, startDate, endDate);
+	}
+	@Override
+	public List<UserTypeChargeDTO> elecChargePeriod(String userId, LocalDate startDate, LocalDate endDate) {
+		return mapper.getElecChargePeriod(userId, startDate, endDate);
 	}
 
 }
