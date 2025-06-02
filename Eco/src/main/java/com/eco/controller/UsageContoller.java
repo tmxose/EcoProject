b37dc8225c4706e16bf77bef83c4fea440ec43f0
GRCOMNE <GRCOMNE@DESKTOP-41EUKUB>
2025-06-02 09:43:45 +0900

    pjh 컨트롤러에 세션의 아이디 값 받아오게 하기

--- Eco/src/main/java/com/eco/controller/UsageContoller.java
+++ Eco/src/main/java/com/eco/controller/UsageContoller.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.eco.domain.UserTypeChargeDTO;
+import com.eco.domain.UserVO;
 import com.eco.service.UsageService;
 
 import lombok.AllArgsConstructor;
@@ -26,16 +27,17 @@ public class UsageContoller {
 	public String usageSelect(Model model, HttpSession session) {
 		log.info("이번 달 나의 사용량 가져오기");
 		// session의 유저 아이디 가져오기
-		String userId = (String)session.getAttribute("currentUserID");
+		UserVO user = (UserVO) session.getAttribute("currentUserInfo");
 		// 이번 달 에너지 사용량 합계
-		model.addAttribute("usage", service.readGasusage("abcd"));
-		model.addAttribute("usage", service.readElecusage("abcd"));
-		
+		model.addAttribute("usage", service.readGasusage(user.getUser_id()));
+		model.addAttribute("usage", service.readElecusage(user.getUser_id()));
+		log.info(user.getUser_id());
 		//가스 상세 내역
-		List<UserTypeChargeDTO> gasUse = service.gasUsageDetail("abcd");
+		List<UserTypeChargeDTO> gasUse = service.gasUsageDetail(user.getUser_id());
 		model.addAttribute("gasUse", gasUse);
+		log.info(gasUse);
 		//전기 상세 내역
-		List<UserTypeChargeDTO> elecUse = service.elecUsageDetail("abcd");
+		List<UserTypeChargeDTO> elecUse = service.elecUsageDetail(user.getUser_id());
 		model.addAttribute("elecUse", elecUse);
 		
 		return "usage";
