58bd82c60103adb779a69df1a1af9c7dec960207
tmxose <tmxose@naver.com>
2025-06-04 09:08:05 +0900

    shj 회원가입시 아이디 중복 방지 처리

--- Eco/src/main/java/com/eco/service/UserService.java
+++ Eco/src/main/java/com/eco/service/UserService.java
@@ -4,11 +4,11 @@ import com.eco.domain.UserVO;
 
 public interface UserService {
 	public void signup(UserVO user);
-	// ��ȸ�Լ��� ������		
-
+	
+	// 로그인 기능 구현
 	public UserVO login(UserVO user);
 	
-	// �Ҽ� �α��� 
+	// 사용자 아이디 존재여부 확인
 	public UserVO findByUserId(String user_id, String user_type);
 	 
 }
