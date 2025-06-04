26806c2816db647b24b2c167401488380ab8dd99
GRCOMNE <GRCOMNE@DESKTOP-41EUKUB>
2025-06-04 15:05:35 +0900

    pjh usage charge mapper parameter 수정

--- Eco/src/main/java/com/eco/mapper/UsageMapper.java
+++ Eco/src/main/java/com/eco/mapper/UsageMapper.java
@@ -3,6 +3,8 @@ package com.eco.mapper;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.eco.domain.ElecUsageVO;
 import com.eco.domain.ElecVO;
 import com.eco.domain.GasUsageVO;
@@ -11,19 +13,19 @@ import com.eco.domain.UserTypeChargeDTO;
 
 public interface UsageMapper {
 	// 이번 달 에너지 사용량 합계
-	public UserTypeChargeDTO getGasUsage(String userId);
+	public UserTypeChargeDTO getGasUsage(@Param("user_id") String userId);
 
-	public UserTypeChargeDTO getElecUsage(String userId);
+	public UserTypeChargeDTO getElecUsage(@Param("user_id") String userId);
 
 	// 당월 에너지 사용 상세 내역
-	public List<UserTypeChargeDTO> getGasUsageDetail(String userId);
+	public List<UserTypeChargeDTO> getGasUsageDetail(@Param("user_id") String userId);
 
-	public List<UserTypeChargeDTO> getElecUsageDetail(String userId);
+	public List<UserTypeChargeDTO> getElecUsageDetail(@Param("user_id") String userId);
 
 	// 지정 기간 에너지 사용 상세 내역
-	public List<UserTypeChargeDTO> getGasUsagePeriod(String userId, LocalDate startDate, LocalDate endDate);
+	public List<UserTypeChargeDTO> getGasUsagePeriod(@Param("user_id") String userId, @Param("start_date") LocalDate startDate, @Param("end_date") LocalDate endDate);
 
-	public List<UserTypeChargeDTO> getElecUsagePeriod(String userId, LocalDate startDate, LocalDate endDate);
+	public List<UserTypeChargeDTO> getElecUsagePeriod(@Param("user_id") String userId, @Param("start_date") LocalDate startDate, @Param("end_date") LocalDate endDate);
 
 	// 지역별 가장 최근 달의 에너지 사용량 합계
 	public List<UserTypeChargeDTO> getUsageLocalAmount();
