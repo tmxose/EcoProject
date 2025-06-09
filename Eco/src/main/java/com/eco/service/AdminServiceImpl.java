d6a5d583f5294d91594d7a6f4596ff338bca15d7
tmxose <tmxose@naver.com>
2025-06-09 12:25:45 +0900

    shj query 예외 처리

--- Eco/src/main/java/com/eco/service/AdminServiceImpl.java
+++ Eco/src/main/java/com/eco/service/AdminServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.eco.domain.ElecUsageVO;
 import com.eco.domain.GasUsageVO;
 import com.eco.domain.UserVO;
+import com.eco.exception.ServiceException;
 import com.eco.mapper.AdminMapper;
 
 import lombok.AllArgsConstructor;
@@ -22,62 +23,97 @@ public class AdminServiceImpl implements AdminService {
 	// 사용자 검색
 	@Override
 	public List<UserVO> searchUsers(String user_nm) {
-		return mapper.selectUers(user_nm);
+		try {
+			return mapper.selectUers(user_nm);
+		} catch (Exception e) {
+			throw new ServiceException("사용자 검색 실패", e);
+		}
 	}
 
 	// 전기 사용내역 조회
 	@Override
 	public List<ElecUsageVO> getElecUsageByUser(int userCd) {
-		return mapper.selectElecUsageByUser(userCd);
+		try {
+			return mapper.selectElecUsageByUser(userCd);
+		} catch (Exception e) {
+			throw new ServiceException("전기 사용내역 조회 실패", e);
+		}
 	}
 
 	// 가스 사용내역 조회
 	@Override
 	public List<GasUsageVO> getGasUsageByUser(int userCd) {
-		return mapper.selectGasUsageByUser(userCd);
+		try {
+			return mapper.selectGasUsageByUser(userCd);
+		} catch (Exception e) {
+			throw new ServiceException("가스 사용내역 조회 실패", e);
+		}
 	}
 
 	// 가스 사용량 등록
 	@Override
 	public boolean insertGas(GasUsageVO vo) {
-		int rows = mapper.insertGasUsage(vo);
-		log.info("insertGasUsage 영향 받은 행 수: " + rows);
-		return rows > 0;
+		try {
+			int rows = mapper.insertGasUsage(vo);
+			return rows > 0;
+		} catch (Exception e) {
+			throw new ServiceException("가스 사용량 등록 실패", e);
+		}
 	}
 
 	// 전기 사용량 등록
 	@Override
 	public boolean insertElec(ElecUsageVO vo) {
-		int rows = mapper.insertElecUsage(vo);
-		return rows > 0;
+		try {
+			int rows = mapper.insertElecUsage(vo);
+			return rows > 0;
+		} catch (Exception e) {
+			throw new ServiceException("전기 사용량 등록 실패", e);
+		}
 	}
 
 	// 전기 수정
 	@Override
 	public boolean updateElec(ElecUsageVO vo) {
-		int affectedRows = mapper.updateElec(vo);
-		return affectedRows > 0;
+		try {
+			int affectedRows = mapper.updateElec(vo);
+			return affectedRows > 0;
+		} catch (Exception e) {
+			throw new ServiceException("전기 사용량 수정 실패", e);
+		}
 	}
 
 	// 전기 삭제
 	@Override
 	public boolean deleteElec(int usage_cd) {
-		int affectedRows = mapper.deleteElec(usage_cd);
-		return affectedRows > 0;
+		try {
+			int affectedRows = mapper.deleteElec(usage_cd);
+			return affectedRows > 0;
+		} catch (Exception e) {
+			throw new ServiceException("전기 사용량 삭제 실패", e);
+		}
 	}
 
 	// 가스 수정
 	@Override
 	public boolean updateGas(GasUsageVO vo) {
-		int affectedRows = mapper.updateGas(vo);
-		return affectedRows > 0;
+		try {
+			int affectedRows = mapper.updateGas(vo);
+			return affectedRows > 0;
+		} catch (Exception e) {
+			throw new ServiceException("가스 사용량 수정 실패", e);
+		}
 	}
 
 	// 가스 삭제
 	@Override
 	public boolean deleteGas(int usage_cd) {
-		int affectedRows = mapper.deleteGas(usage_cd);
-		return affectedRows > 0;
+		try {
+			int affectedRows = mapper.deleteGas(usage_cd);
+			return affectedRows > 0;
+		} catch (Exception e) {
+			throw new ServiceException("가스 사용량 삭제 실패", e);
+		}
 	}
 
 }
