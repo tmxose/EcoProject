285100539dc564e8b65966409e4a3fda2611f391
tmxose <tmxose@naver.com>
2025-06-10 15:50:39 +0900

    shj @value 값 읽어오는부분 컨트롤러-> 서비스 로변경

--- Eco/src/main/java/com/eco/service/OAuthService.java
+++ Eco/src/main/java/com/eco/service/OAuthService.java
@@ -3,6 +3,13 @@ package com.eco.service;
 import com.eco.domain.UserVO;
 
 public interface OAuthService {
-	 public String getGoogleLoginUrl();
-	 public UserVO processGoogleLogin(String code);
+	// Google OAuth
+	public String getGoogleLoginUrl();
+
+	public UserVO processGoogleLogin(String code);
+
+	// Naver OAuth
+	public String getNaverLoginUrl();
+
+	public UserVO processNaverLogin(String code, String state);
 }
